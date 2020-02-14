87f590bbfacb9268f9cf15d57fb666b531d16568
Hugo Castro de Deco (WorkDesktop) <hugodeco@sufficit.com.br>
2020-02-14 16:51:10 -0300

    coisas desnecessárias;

---
+++ Acesso/LoginParameters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sufficit.Acesso
+{
+    public class LoginParameters
+    {
+        //[Required]
+        public string UserName { get; set; }
+
+        //[Required]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ Acesso/RegisterParameters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sufficit.Acesso
+{
+    public class RegisterParameters
+    {
+        //[Required]
+        public string UserName { get; set; }
+
+        //[Required]
+        public string Password { get; set; }
+
+        //[Required]
+        //[Compare(nameof(Password), ErrorMessage = "Passwords do not match")]
+        public string PasswordConfirm { get; set; }
+    }
+}
