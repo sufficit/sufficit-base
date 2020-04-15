dd934ce3804b407527dd9bc206d09fc21f2a5a7e
Hugo Castro de Deco (WorkDesktop) <hugodeco@sufficit.com.br>
2020-04-14 22:34:16 -0300

    fornecedor;

---
+++ Telefonia/Fornecedor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sufficit.Telefonia
+{
+    public class Fornecedor
+    {
+        public static Guid Padrao = Guid.Empty;
+        public static Guid GTGI0800 = Guid.Parse("f9fc7cbb-7290-4fba-95cb-ff06bf2aafc6");        
+    }
+}
