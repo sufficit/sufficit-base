385ddc128b6a2e6e5fb8b6c732502fc0d2d87b33
Hugo Castro de Deco (WorkDesktop) <hugodeco@sufficit.com.br>
2020-12-23 11:43:41 -0300

    dial out trunk;

---
+++ Telefonia/DialOutTrunk.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sufficit.Telefonia
+{
+    /// <summary>
+    /// Tronco usado para discagem externa
+    /// </summary>
+    public class DialOutTrunk
+    {
+        public string tech { get; set; }
+        public string title { get; set; }
+        public int timeout { get; set; }
+        public string options { get; set; }
+        public string url { get; set; }
+        public string prepend { get; set; }
+        public int remove { get; set; }
+
+        public string model { get; set; }
+        public bool idforward { get; set; }
+    }
+}
