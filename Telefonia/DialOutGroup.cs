d2065a5c2ab7d6cbe5ac46e3c813d96791ca4594
Hugo Castro de Deco (WorkDesktop) <hugodeco@sufficit.com.br>
2021-01-14 10:17:16 -0300

    ajustes;

---
+++ Telefonia/DialOutGroup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sufficit.Telefonia
+{
+    /// <summary>
+    /// Tronco usado para discagem externa
+    /// </summary>
+    public class DialOutGroup
+    {
+        public string group { get; set; }
+        public int prio { get; set; }
+        public string title { get; set; }
+        public string tech { get; set; }
+        public int timeout { get; set; }
+        public string options { get; set; }
+        public string url { get; set; }
+        public string prepend { get; set; }
+        public int remove { get; set; }
+
+        public string model { get; set; }
+        public bool idforward { get; set; }
+        public string comments { get; set; }
+    }
+}
--- Telefonia/DialOutTrunk.cs
+++ Telefonia/DialOutTrunk.cs
@@ -11,18 +11,10 @@ namespace Sufficit.Telefonia
     /// </summary>
     public class DialOutTrunk
     {
-        public string group { get; set; }
-        public string title { get; set; }
-        public int prio { get; set; }
-        public string tech { get; set; }
-        public int timeout { get; set; }
-        public string options { get; set; }
-        public string url { get; set; }
-        public string prepend { get; set; }
-        public int remove { get; set; }
+        public Guid context { get; set; }
 
-        public string model { get; set; }
-        public bool idforward { get; set; }
-        public string comments { get; set; }
+        public string title { get; set; }
+
+        public int channels { get; set; }
     }
 }
