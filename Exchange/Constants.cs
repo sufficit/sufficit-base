e013faeb4a0eeaf339579a05d728528618de4392
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2021-05-22 17:47:09 -0300

    regex de e-mail

---
+++ Exchange/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sufficit.Exchange
+{
+    public class Constants
+    {
+        public const string EMAILREGEX = @"[a-z0-9][a-z0-9\.\-_]+[a-z0-9]@[-a-z0-9]*\.[\.?[a-z0-9]*";
+    }
+}
