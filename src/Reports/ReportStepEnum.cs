59b44db0a1c213d8e1d314ef7c0817bcc19fe5e0
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2021-08-12 23:43:04 -0300

    report steps

---
+++ src/Reports/ReportStepEnum.cs
@@ -0,0 +1,27 @@
+﻿namespace Sufficit.Reports
+{
+    public enum ReportStepEnum
+    {
+        /// <summary>
+        /// Estado desconhecido
+        /// </summary>
+        UNKNOWN,
+
+        /// <summary>
+        /// Parametros básicos aceitos para iniciar (relatório instanciado)
+        /// </summary>
+        ACCEPTED = 10,
+
+        /// <summary>
+        /// Fase de aquisição de dados para realizar os calculos
+        /// </summary>
+        FETCHING = 20,
+
+        CONSOLIDING = 30,
+        FILTERING = 40,
+        PROCESSING = 50,
+        FORMATTING = 90,
+        FINISHED = 100,
+        ERROR
+    }
+}
