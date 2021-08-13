1911e854f5e4485a8b736a80fb4ebb19cbcab63c
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2021-08-12 23:42:30 -0300

    report progress;

---
+++ src/Reports/IReportProgress.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sufficit.Reports
+{
+    /// <summary>
+    /// Interface para informar o progresso de geração de um relatório
+    /// </summary>
+    public interface IReportProgress
+    {
+        /// <summary>
+        /// Progresso de geração em porcentagem (sempre positivo)
+        /// </summary>
+        uint Percentage { get; }
+
+        /// <summary>
+        /// Descrição sobre o estado atual do processo
+        /// </summary>
+        string Status { get; }
+
+        /// <summary>
+        /// Etapa atual do processo
+        /// </summary>
+        ReportStepEnum Step { get; }
+    }
+}
