a1f9ea9fafa3471307704efc44a497cf892304d7
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2021-08-12 23:41:43 -0300

    new report model;

--- src/Reports/Report.cs
+++ src/Reports/Report.cs
@@ -4,19 +4,40 @@ using System.Text;
 
 namespace Sufficit.Reports
 {
-    public abstract class Report
+    public abstract class Report : IReportProgress
     {
+        #region IMPLEMENT IREPORTPROGRESS
+                
+        uint IReportProgress.Percentage => progress.Percentage;
+        string IReportProgress.Status => progress.Status;
+        ReportStepEnum IReportProgress.Step => progress.Step;
+
+        #endregion
+        #region CONSTRUCTOR
+
         public Report(IReportParameters parameters)
         {
-            if (parameters == null) 
+            if (parameters == null)
                 throw new ArgumentNullException("parameters");
 
+            // Instanciando controle de progresso
+            this.progress = new ReportProgress();
+            
             this.DTCreation = DateTime.UtcNow;
             this.IDContext = parameters.IDContext;
             this.DTStart = parameters.DTStart;
             this.DTEnd = parameters.DTEnd;
         }
-        
+
+        #endregion
+        #region PROPERTIES
+
+        protected ReportProgress progress;
+        public IReportProgress Progress => progress;
+
+        #endregion
+        #region DEFAULT PARAMETERS
+
         /// <summary>
         /// Contexto de representação do relatório, cliente ou usuário
         /// </summary>
@@ -37,5 +58,7 @@ namespace Sufficit.Reports
         /// Usado somente para controle
         /// </summary>
         public DateTime DTCreation { get; }
+
+        #endregion
     }
 }
