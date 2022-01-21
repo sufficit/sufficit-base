5f11c70cf5fafc6bc452132ab8e805a9213deed6
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2022-01-20 23:40:48 -0300

    erro de portugues

--- src/Telephony/ICallSearchParameters.cs
+++ src/Telephony/ICallSearchParameters.cs
@@ -20,35 +20,35 @@ namespace Sufficit.Telephony
         DateTime Start { get; }
 
         /// <summary>
-        /// (Opicional) Data/Hora de término do intervalo de busca <br />
+        /// (Opcional) Data/Hora de término do intervalo de busca <br />
         /// Caso não seja preenchido, o resultado trará todos os registro apartir da data/hora de início
         /// </summary>
         DateTime? End { get; }
 
         /// <summary>
-        /// (Opicional) Traz os resultados no seguinte fuso / região de horário <br />
+        /// (Opcional) Traz os resultados no seguinte fuso / região de horário <br />
         /// O padrão é trazer tudo em GMT
         /// </summary>
         RegionEnum? Region { get; }
 
         /// <summary>
-        /// (Opicional) Filtra a pesquisa por apenas as DIDs selecionadas, portanto, somente chamadas de entrada
+        /// (Opcional) Filtra a pesquisa por apenas as DIDs selecionadas, portanto, somente chamadas de entrada
         /// </summary>
         string[] DIDs { get; }
 
         /// <summary>
-        /// (Opicional) Filtra por apenas chamadas tarifadas, que tiveram ou podem gerar custos <br />
+        /// (Opcional) Filtra por apenas chamadas tarifadas, que tiveram ou podem gerar custos <br />
         /// Valor padrão (false)
         /// </summary>
         bool Billed { get; }
 
         /// <summary>
-        /// (Opicional) Filtra por apenas chamadas atendidas, que foram marcadas como atendidas pela central telefonica
+        /// (Opcional) Filtra por apenas chamadas atendidas, que foram marcadas como atendidas pela central telefonica
         /// </summary>
         bool Answered { get; }
 
         /// <summary>
-        /// (Opicional) Limite de resultados na consulta (aproximado)
+        /// (Opcional) Limite de resultados na consulta (aproximado)
         /// </summary>
         int Limit { get; }
     }
