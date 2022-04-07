6680a121ac3c21d51e0f00233a0f5dc1b66ea197
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2022-04-07 00:17:09 -0300

    userclaim;

---
+++ src/Identity/UserClaim.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sufficit.Identity
+{
+    /// <summary>
+    /// Database representation of a user claim
+    /// </summary>
+    public class UserClaim
+    {
+        public int Id { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public string ClaimType { get; set; }
+
+        public string ClaimValue { get; set; }
+    }
+}
