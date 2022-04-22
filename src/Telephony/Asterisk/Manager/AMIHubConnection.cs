71e8d81bac5f494c30e04b70f652857beb7d404a
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2022-04-22 19:55:51 -0300

    hub connection via api

---
+++ src/Telephony/Asterisk/Manager/AMIHubConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sufficit.Telephony.Asterisk.Manager
+{
+    public class AMIHubConnection
+    {
+        /// <summary>
+        /// Servidor por onde foi processada essa parte da chamada
+        /// </summary>
+        [Key, Column("endpoint"), DefaultValue(""), MaxLength(255), Required]
+        public string Endpoint { get; set; }
+    }
+}
