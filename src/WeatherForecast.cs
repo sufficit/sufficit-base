89d5fa3e6fa19465df86822658189af6410f267c
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2022-06-28 11:55:56 -0300

    nuget update key;

--- src/WeatherForecast.cs
+++ src/WeatherForecast.cs
@@ -4,11 +4,14 @@ using System.Text;
 
 namespace Sufficit
 {
+    /// <summary>
+    /// Testing queries
+    /// </summary>
     public class WeatherForecast
     {
         public DateTime Date { get; set; }
         public int TemperatureC { get; set; }
         public int TemperatureF { get; set; }
-        public string Summary { get; set; }
+        public string? Summary { get; set; }
     }
 }
