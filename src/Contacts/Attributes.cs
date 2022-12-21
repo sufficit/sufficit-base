479f8bf4fc433e61c6a9fd8f7cb4e0fb5df251f3
Hugo Castro de Deco (Notebook) <hugodeco@hotmail.com>
2022-12-20 22:35:47 -0300

    attributes keys

---
+++ src/Contacts/Attributes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sufficit.Contacts
+{
+    /// <summary>
+    /// Default contact attributes keys
+    /// </summary>
+    public struct Attributes
+    {
+        /// <summary>
+        /// Key for title attribute
+        /// </summary>
+        public const string Title = "titulo";
+    }
+}
