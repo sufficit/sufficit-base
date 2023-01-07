323487fb6634480e0203d5cb18ea0f9f159499f5
Hugo Castro de Deco (Notebook) <hugodeco@hotmail.com>
2023-01-07 19:23:18 -0300

    did updates

--- src/Telephony/DirectInwardDialingV1.cs
+++ src/Telephony/DirectInwardDialingV1.cs
@@ -18,37 +18,65 @@ namespace Sufficit.Telephony
         [JsonPropertyName("id")]
         public Guid Id { get; set; }
 
-        [JsonPropertyName("provider")]
+        [JsonPropertyName("providerid")]
         public Guid ProviderId { get; set; }
 
-        [JsonPropertyName("context")]
+        [JsonPropertyName("contextid")]
         public Guid ContextId { get; set; }
 
         [JsonPropertyName("billed")]
         public bool Billed { get; set; }
 
-        [JsonPropertyName("update")]
-        public DateTime Update { get; set; }
-
         [JsonPropertyName("description")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull | JsonIgnoreCondition.WhenWritingDefault)]
         public string? Description { get; set; }
 
         /// <summary>
         /// Timestamp for the moment of this object was contracted, since billing start to count
         /// </summary>
         [JsonPropertyName("register")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public DateTime Register { get; set; }
 
+        [JsonPropertyName("maxchannels")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public int MaxChannels { get; set; }
 
+        /// <summary>
+        /// Regex to filter source calls caller ids <br />
+        /// Only permit calls where does NOT match the criterias
+        /// </summary>
+        [JsonPropertyName("filter")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull | JsonIgnoreCondition.WhenWritingDefault)]
+        public string? Filter { get; set; }
 
-        public DateTime cadastro { get => Register; set => Register = value; }
-        public string descricao { get => Description ?? string.Empty; set => Description = value; }
+        /// <summary>
+        /// Prepend caller id names
+        /// </summary>
+        /// <example>#COMERCIAL</example>
+        [JsonPropertyName("tags")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull | JsonIgnoreCondition.WhenWritingDefault)]
+        public string? Tags { get; set; }
+
+        [JsonPropertyName("asterisk")]
+        public string Asterisk { get; set; } = default!;
+
+        [JsonPropertyName("ownerid")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull | JsonIgnoreCondition.WhenWritingDefault)]
+        public Guid? OwnerId { get; set; }
+
+        [JsonPropertyName("update")]
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.Always)]
+        public DateTime Update { get; set; }
+
+        #region NOT THREATED YET
 
         public string tdestino { get; set; } = default!;
-        public string origem { get; set; } = default!;
-        public string tags { get; set; } = default!;
         public string dstclasse { get; set; } = default!;
         public string dstid { get; set; } = default!;
+
+        #endregion
 
         #region IMPLEMENTING INTERFACE DIRECT INWARD DIALING
 
@@ -62,7 +90,7 @@ namespace Sufficit.Telephony
         Guid IIndex.Id => Id;
         Guid IIndexable.ContextId => ContextId;
         string IIndexable.Title => Extension;
-        string IIndexable.Description => descricao;
+        string IIndexable.Description => Description ?? string.Empty;
 
         #endregion
     }
