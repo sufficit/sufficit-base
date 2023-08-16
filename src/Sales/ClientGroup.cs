15aa0d938aa94f3c627e249709c64f53215e3050
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2023-08-16 09:55:48 -0300

    testing sales client group

---
+++ src/Sales/ClientGroup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sufficit.Sales
+{
+    public class ClientGroup
+    {
+        public Guid GroupId { get; set; }
+
+        public ICollection<Guid> MembersIds { get; set; } 
+            = new HashSet<Guid>();   
+    }
+}
---
+++ src/Sales/ClientGroupMember.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sufficit.Sales
+{
+    public class ClientGroupMember
+    {
+        public Guid GroupId { get; set; }
+
+        public Guid MemberId { get; set; }
+    }
+}
