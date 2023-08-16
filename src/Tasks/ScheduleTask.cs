f459256b3c75cfd882beea67ee06adbc95760278
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2023-08-16 09:53:49 -0300

    schedule tasks

--- src/Tasks/ScheduleTask.cs
+++ src/Tasks/ScheduleTask.cs
@@ -1,21 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace Sufficit.Tasks
 {
     public class ScheduleTask : CronSchedule
     {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public Guid Id { get; set; }
 
+        [JsonRequired]
         public string Method { get; set; } = default!;
 
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull | JsonIgnoreCondition.WhenWritingDefault)]
         public object? Args { get; set; }
 
         public bool Active { get; set; }
+            = true;
 
         public bool Locked { get; set; }
+            = false;
 
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public DateTime Timestamp { get; set; }
     }
 }
