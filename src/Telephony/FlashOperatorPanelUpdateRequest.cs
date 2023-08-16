c4da8c861a2482f1fd13699cf1eaad8b9ef7dca6
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2023-08-16 09:55:26 -0300

    flash operator panel

---
+++ src/Telephony/FlashOperatorPanelUpdateRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sufficit.Telephony
+{
+    public class FlashOperatorPanelUpdateRequest
+    {
+        public Guid ContextId { get; set; }
+
+        public bool Queues { get; set; }
+            = true;
+
+        public bool Force { get; set; }
+            = true;
+
+        public bool Ping { get; set; }
+            = false;
+
+        public uint TimeOutMs { get; set; }
+            = 15000;
+    }
+}
