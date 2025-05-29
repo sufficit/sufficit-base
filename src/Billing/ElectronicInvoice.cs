b143dd98eac680d41887eab4e4d46ed486f8d7ee
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2025-05-29 19:12:16 -0300

    Adiciona suporte a informações de origem de faturas
    Implementa a classe `EletronicInvoiceSourceInfo` para armazenar dados sobre a origem das faturas eletrônicas. Atualiza a classe `ElectronicInvoice` com novas propriedades e atributos de serialização JSON.
    
    Configura o mapeamento da nova classe no Entity Framework em `ModelBuilderHelper.cs`.
    
    Atualiza o controlador `ElectronicInvoiceController` para remover arquivos PDF e XML associados a faturas eletrônicas.
    
    Registra o novo controlador no contêiner de injeção de dependência em `AddSufficitBilling`.

--- src/Billing/ElectronicInvoice.cs
+++ src/Billing/ElectronicInvoice.cs
@@ -10,17 +10,26 @@ namespace Sufficit.Billing
     /// </summary>
     public class ElectronicInvoice
     {
+        [JsonPropertyName("id")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public Guid Id { get; set; }
 
+        [JsonPropertyName("contextid")]
         public Guid ContextId { get; set; }
 
+        [JsonPropertyName("source")]
+        public EletronicInvoiceSourceInfo Source { get; set; } = new EletronicInvoiceSourceInfo();
+
+        [JsonPropertyName("creation")]
         public DateTimeOffset Creation { get; set; }
 
+        [JsonPropertyName("value")]
         public decimal Value { get; set; }
 
+        [JsonPropertyName("xml")]
         public Guid? XML { get; set; }
 
+        [JsonPropertyName("pdf")]
         public Guid? PDF { get; set; }
     }
 }
---
+++ src/Billing/EletronicInvoiceSourceInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Sufficit.Billing
+{
+    /// <summary>
+    ///     Source information for electronic invoices, such as the system that generated the invoice, its external ID, and current status.
+    /// </summary>
+    public class EletronicInvoiceSourceInfo
+    {
+        /// <summary>
+        ///     System that generated the invoice, e.g. "ASAAS", "GERENCIANET", etc.
+        /// </summary>
+        /// <remarks>* Upstring normalized</remarks>
+        [JsonPropertyName("system")]
+        public string System { get; set; } = default!;
+
+        /// <summary>
+        ///     External Id, used to identify the invoice on source systems
+        /// </summary>
+        [JsonPropertyName("id")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull | JsonIgnoreCondition.WhenWritingDefault)]
+        public string? Id { get; set; }
+
+        /// <summary>
+        ///     Current status of the invoice on the source system
+        /// </summary>
+        [JsonPropertyName("status")]
+        public string Status { get; set; } = default!;
+    }
+}
