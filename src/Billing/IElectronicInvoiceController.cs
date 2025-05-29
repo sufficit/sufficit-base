755d5362222192de032eae115155eea07d05c3cd
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2025-05-29 12:42:25 -0300

    Adiciona controle de faturas eletrônicas
    Implementa a interface `IElectronicInvoiceController` com métodos para buscar, adicionar, atualizar e remover faturas eletrônicas. Adiciona a seção `Billing` no `APIClientService.cs` para acesso ao novo controlador de faturas. Atualiza `BillingControllerSection.cs` para incluir `EletronicInvoice`. Implementa `EletronicInvoiceControllerSection.cs` com a lógica necessária e anotações de autorização para controle de acesso.

---
+++ src/Billing/IElectronicInvoiceController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sufficit.Billing
+{
+    public interface IElectronicInvoiceController
+    {        
+        /// <summary>
+        ///     Get item by id, with files
+        /// </summary>
+        Task<ElectronicInvoice?> ById (Guid id, CancellationToken cancellationToken);
+
+        Task<IEnumerable<ElectronicInvoice>> Search(ElectronicInvoiceSearchParameters parameters, CancellationToken cancellationToken);
+
+        /// <summary>
+        ///     Add or update an item. If already exists, it will be updated.   
+        /// </summary>
+        Task<ElectronicInvoice> AddOrUpdate(ElectronicInvoice item, CancellationToken cancellationToken);
+
+        /// <summary>
+        ///     Remove an item by id, if exists
+        /// </summary>
+        Task<int> Remove(Guid id, CancellationToken cancellationToken);
+    }
+}
