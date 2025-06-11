e7cc43d663db26e550a9ab90f7aea2a06f6119cc
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2025-06-11 10:32:32 -0300

    Adiciona comentários XML à enumeração CronScheduleType
    Adicionados comentários XML que descrevem a enumeração `CronScheduleType`, detalhando seus componentes, como minutos, horas, dias do mês, meses e dias da semana. Esses comentários melhoram a documentação do código e facilitam a compreensão do propósito da enumeração.

--- src/CronScheduleType.cs
+++ src/CronScheduleType.cs
@@ -4,6 +4,12 @@ using System.Text;
 
 namespace Sufficit
 {
+    /// <summary>
+    /// Specifies the type of schedule used in a cron expression.
+    /// </summary>
+    /// <remarks>This enumeration represents the different components of a cron schedule, such as minutes,
+    /// hours,  days of the month, months, and weekdays. It can be used to define or interpret cron expressions  for
+    /// scheduling tasks.</remarks>
     public enum CronScheduleType
     {
         MINUTES,
