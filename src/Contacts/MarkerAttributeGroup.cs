58881041c6ba5cefbb3a29afcf7dd7d240f52ca9
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2025-07-03 19:10:48 -0300

    Adicionar estrutura MarkerAttributeGroup com propriedades
    Implementada a estrutura `MarkerAttributeGroup` no arquivo `MarkerAttributeGroup.cs`, que representa um grupo de atributos de marcador. A estrutura inclui um identificador de marcador (`Marker`) e uma contagem de uso (`Count`), além de novas importações necessárias.

---
+++ src/Contacts/MarkerAttributeGroup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sufficit.Contacts
+{
+    /// <summary>
+    /// Represents a group of marker attributes, including a marker identifier and its associated usage count.
+    /// </summary>
+    /// <remarks>This struct is typically used to store and manage information about markers, such as their
+    /// unique identifier and the number of occurrences or instances associated with them.</remarks>
+    public struct MarkerAttributeGroup
+    {
+        public string Marker { get; set; }
+
+        public uint Count { get; set; }
+    }
+}
