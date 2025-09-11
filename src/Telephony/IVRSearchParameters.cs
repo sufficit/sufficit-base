657c9efe9c8ab57c0c3e0712802cf4dc035c4e4c
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2025-09-11 19:23:32 -0300

    Remove IVRControllerInterface and update IVRSearchParameters with additional properties and documentation

--- src/Telephony/IVRSearchParameters.cs
+++ src/Telephony/IVRSearchParameters.cs
@@ -6,7 +6,7 @@ namespace Sufficit.Telephony
     /// <summary>
     /// Interactive Voice Response search parameters
     /// </summary>
-    public class IVRSearchParameters
+    public class IVRSearchParameters : ILimit
     {
         /// <summary>
         /// Default name for IVR Id parameter
@@ -22,6 +22,23 @@ namespace Sufficit.Telephony
         /// <example>00000000-0000-0000-0000-000000000000</example>
         public Guid? ContextId { get; set; }
 
-        public string? Title { get; set; }
+        /// <summary>
+        /// IVR title, can be partial or full match
+        /// </summary>
+        public TextFilter? Title { get; set; }
+
+        /// <summary>
+        /// Extension to be used when direct dialing to an IVR
+        /// </summary>
+        public string? Extension { get; set; }
+
+        /// <summary>
+        /// FreePBX identifier, if any
+        /// Used to link with FreePBX system, if any
+        /// </summary>
+        public int? FPBXId { get; set; }
+
+        /// <inheritdoc cref="ILimit.Limit"/>
+        public uint? Limit { get; set; }
     }
 }
